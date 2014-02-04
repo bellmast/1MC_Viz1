ea26f4b310009211c4889492a1ce639440e61881
bellmast <jbm444@gmail.com>
2014-02-04 16:38:49 -0600

    Update Comparison.js

--- Comparison.js
+++ Comparison.js
@@ -61,12 +61,14 @@ function drawNetwork(data) {
     ycoordArray = []
     radiiArray = []
 
+    count = 0
+
     for (i = 2; i < arrayCount; i++) {
         h = myArray[i]
         radius = h*2
 
-        
-        xcoord = (slots*(i-1))          
+        count += 1
+        xcoord = (slots*count)          
 
         orgSet.push(paper.circle(xcoord, midY, radius))
         textSet.push(paper.text(xcoord, midY+radius, orgNames[i]))
