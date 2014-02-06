dd5dae6b10bfa6a1727258b29a56c2ec52122deb
bellmast <jbm444@gmail.com>
2014-02-06 16:07:47 -0600

    Update Comparison.js

--- Comparison.js
+++ Comparison.js
@@ -128,14 +128,14 @@ function drawNetwork(data) {
         masterCirclePackingArray.push(0)
                         
         if (data[i][0] == "FT") {
-            FTSet.push(paper.circle(avgxCoord, avgyCoord, ourRadius).attr({fill:"#918070", stroke:"#FFFFFF"}))
+            FTSet.push(paper.circle(avgxCoord, avgyCoord, ourRadius).attr({fill:"#918070"}))
             for (p = 0; p < currentLength; p++) {                
                 linesFTSet.push(
                     paper.path("M"+avgxCoord+" "+avgyCoord+"L"+currentxArray[p]+" "+currentyArray[p]).attr({"stroke-width": ".5", "stroke-dasharray":"--"})
                 )
             }
         } else if (data[i][0] == "1MC") {
-            oneMCSet.push(paper.circle(avgxCoord, avgyCoord, ourRadius).attr({fill:"#F58823", stroke:"#FFFFFF"}))
+            oneMCSet.push(paper.circle(avgxCoord, avgyCoord, ourRadius).attr({fill:"#F58823"}))
             for (p = 0; p < currentLength; p++) {                
                 linesoneMCSet.push(
                     paper.path("M"+avgxCoord+" "+avgyCoord+"L"+currentxArray[p]+" "+currentyArray[p]).attr({"stroke-width": ".4"})
@@ -149,7 +149,7 @@ function drawNetwork(data) {
     linesSet.push(linesFTSet, linesoneMCSet);
     allSet.push(FTSet, oneMCSet)
 
-    allSet.attr({stroke:"none"})
+    allSet.attr({stroke:"#FFFFFF"})
         
     oneMCSet.hide()
     FTSet.hide()
